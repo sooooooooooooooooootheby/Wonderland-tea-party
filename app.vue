1df0a4590d37efce6723a8e88f4bc061e72b58e9
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-01-15 17:41:34 +0800

    添加 站点头像以及标题

--- app.vue
+++ app.vue
@@ -10,6 +10,18 @@
     </a-config-provider>
 </template>
 
-<script setup></script>
+<script setup>
+useHead({
+	title: "wonderland-tea-party",
+    meta: [{ name: "description", content: "这是一个ai聊天站点" }],
+	link: [
+		{ rel: "shortcut icon", href: "/logo_small.webp" },
+		{ rel: "apple-touch-icon", href: "/logo_small.webp" },
+	],
+	titleTemplate(titleChunk) {
+		return titleChunk ? `${titleChunk} - s22y's webSite` : "s22y's webSite";
+	},
+});
+</script>
 
 <style lang="scss" scoped></style>
